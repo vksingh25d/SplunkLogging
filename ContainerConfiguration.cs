1a3797b933873af864fcbd7d8b089e996fb0ebeb
Singh <Vikas.Singh@ap.jll.com>
2022-05-04 17:06:43 +0530

    initial comment

---
+++ ContainerConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Common;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Splunk_Logging
+{
+    internal static class ContainerConfiguration
+    {
+        public static IServiceProvider Configure()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            var s = LogManager.LoadConfiguration("nLog.xml");
+
+            //InternalLogger.LogToConsole = true;
+            //InternalLogger.LogLevel = NLog.LogLevel.Trace;
+
+            serviceCollection.AddLogging(o =>
+            {
+                o.ClearProviders();
+                o.AddNLog();
+            }).Configure<LoggerFilterOptions>(c => c.MinLevel = Microsoft.Extensions.Logging.LogLevel.Trace);
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Splunk.Logging;
+using Newtonsoft.Json.Linq;
+
+namespace Splunk_Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = ContainerConfiguration.Configure();
+            var logger = serviceProvider.GetService<ILogger<Program>>();
+            logger.LogInformation("Test splunk log : " + DateTime.Now + " , Hello Vikas ");
+        }
+    }
+}
